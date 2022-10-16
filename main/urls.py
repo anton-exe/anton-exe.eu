41c081a4e0151c15985b15c421358f0ec0785b94
Anton Sniec <antek@sniec.pl>
2022-10-16 11:48:45 +0000

    other changes

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,7 @@ app_name = "main"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('sociallinks', views.render_template, {"template_name": "main/sociallinks.html"}, "sociallinks"),
+    path('sociallinks.html', views.render_template, {"template_name": "main/sociallinks.html"}, "sociallinks"),
     path('<str:pk>', views.TextPageView.as_view(), name='textPage'),
 ]
+
